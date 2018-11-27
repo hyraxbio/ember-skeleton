755473838b61605dd145736b1bef75cb81228088
Andrew Paterson <andrew@hyraxbio.co.za>
2018-11-27 16:54:27 +0200

    Updated basic layouts to use flexbox.

--- tests/dummy/app/components/layouts/footer/footer-content.js
+++ tests/dummy/app/components/layouts/footer/footer-content.js
@@ -1,6 +1,5 @@
 import Component from '@ember/component';
-import layout from '../templates/components/footer-content';
 
 export default Component.extend({
-  layout
+
 });
--- tests/dummy/app/components/main-nav-items.js
+++ tests/dummy/app/components/main-nav-items.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
-import layout from '../templates/components/main-nav-items';
+// import layout from '../templates/components/main-nav-items';
 
 export default Component.extend({
-  layout
+  // layout,
+  tagName: '',
 });
