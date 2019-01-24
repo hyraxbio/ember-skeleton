bcee5dae7eab22b6e1f29e52c1ee05f027940c0f
Andrew Paterson <andrew@hyraxbio.co.za>
2019-01-24 21:22:03 +0200

    various updates.

--- app/helpers/ember-skeleton/theme-color-string.js
+++ app/helpers/ember-skeleton/theme-color-string.js
@@ -1,8 +1,8 @@
 import { helper } from '@ember/component/helper';
 import themeColorStringUtil from 'ember-skeleton/utils/ember-skeleton/theme-color-string';
 
-export function hyraxEmberAssetsThemeColorString(params) {
-  return themeColorStringUtil(params[0]);
+export function hyraxEmberAssetsThemeColorString(params, hash) {
+  return themeColorStringUtil(params[0], hash);
 }
 
 export default helper(hyraxEmberAssetsThemeColorString);
