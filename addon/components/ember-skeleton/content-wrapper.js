bf38f4bfaa1c5548eeccdb896777336822f99382
Andrew Paterson <andrew@hyraxbio.co.za>
2019-06-18 14:31:56 +0100

    Reorganised styles.

--- addon/components/ember-skeleton/content-wrapper.js
+++ addon/components/ember-skeleton/content-wrapper.js
@@ -2,7 +2,5 @@ import Component from '@ember/component';
 import layout from '../../templates/components/ember-skeleton/content-wrapper';
 
 export default Component.extend({
-  layout,
-  tagName: 'div',
-  classNames: ['container', 'section-padding-x', 'section-padding-y'],
+  layout
 });
