b822cfab96e38eb9e186ef89ec0528b9a6495638
Andrew Paterson <andrew@hyraxbio.co.za>
2019-07-02 10:58:34 +0200

    Various styling updates, mostly layout based.

--- addon/components/ember-skeleton/content-wrapper.js
+++ addon/components/ember-skeleton/content-wrapper.js
@@ -2,5 +2,6 @@ import Component from '@ember/component';
 import layout from '../../templates/components/ember-skeleton/content-wrapper';
 
 export default Component.extend({
-  layout
+  layout,
+  classNames: ['padding-xl', 'content-container']
 });
