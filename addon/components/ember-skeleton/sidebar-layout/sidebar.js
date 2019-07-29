16fb202af55c2b4378ece8932d3c4c3e17872722
Andrew Paterson <andrew@hyraxbio.co.za>
2019-07-29 15:33:15 +0200

    Refactored to use sidebar component insteasd of main-nav.

---
+++ addon/components/ember-skeleton/sidebar-layout/sidebar.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import layout from '../../../templates/components/ember-skeleton/sidebar-layout/sidebar';
+
+export default Component.extend({
+  layout
+});
--- addon/services/ember-skeleton.js
+++ addon/services/ember-skeleton.js
@@ -4,5 +4,6 @@ export default Service.extend({
   headerComponent: 'ember-skeleton/header',
   loadingComponent: 'ember-skeleton/loading-page',
   footerComponent: 'ember-skeleton/footer',
-  mainNavItemsComponent: 'main-nav-items'
+  mainNavItemsComponent: 'main-nav-items',
+  sidebarComponent: 'ember-skeleton/sidebar-layout/sidebar'
 });
