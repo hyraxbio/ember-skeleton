4cd8c412afb039f3a5ead6d2c06fe0efc5b69b4b
Andrew Paterson <andrew.paterson9@gmail.com>
2021-09-08 11:42:10 +0200

    Update table styles

--- addon/components/ember-skeleton/status-icon-string.js
+++ addon/components/ember-skeleton/status-icon-string.js
@@ -2,5 +2,6 @@ import Component from '@ember/component';
 import layout from '../../templates/components/ember-skeleton/status-icon-string';
 
 export default Component.extend({
-  layout
+  layout,
+  tagName: ''
 });
