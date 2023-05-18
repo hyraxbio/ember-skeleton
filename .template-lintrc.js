6622137b06a00854fc979e67280be05d64339c56
Andrew Paterson <andrew.paterson9@gmail.com>
2023-05-18 16:10:45 +0200

    Address no-implicit-this deprecations

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -3,6 +3,22 @@
 module.exports = {
   extends: 'recommended',
   rules: {
-    // 'attribute-indentation': false
-  }
+    'attribute-indentation': false,
+    'no-implicit-this': {
+      allow: [
+        'ember-skeleton/array-join',
+        'ember-skeleton/custom-inflector',
+        'ember-skeleton/custom-sort-objects',
+        'ember-skeleton/humanise-string',
+        'ember-skeleton/in-array',
+        'ember-skeleton/odd-or-even',
+        'ember-skeleton/readable-file-size',
+        'ember-skeleton/replace-string',
+        'ember-skeleton/robotise-string',
+        'ember-skeleton/string-split',
+        'ember-skeleton/theme-color-string',
+        'ember-skeleton/theme-icon',
+      ],
+    },
+  },
 };
