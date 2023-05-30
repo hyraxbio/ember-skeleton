1a03b01580d0c69c7686b336c336fc20b098efd1
Andrew Paterson <andrew.paterson9@gmail.com>
2023-05-30 12:05:49 +0200

    Don't update find prop on hash for replace string helper.

--- addon/helpers/ember-skeleton/replace-string.js
+++ addon/helpers/ember-skeleton/replace-string.js
@@ -2,26 +2,32 @@ import { helper } from '@ember/component/helper';
 
 export function replaceString(params, hash) {
   var string = params[0];
-  if (!string) { return; }
-  if (typeof(string) !== 'string') { return; }
+  if (!string) {
+    return;
+  }
+  if (typeof string !== 'string') {
+    return;
+  }
   var final;
+  let find;
   var searchArray = [];
   if (hash.advanced) {
     searchArray = hash.advanced;
   } else if (params[1]) {
     searchArray[0] = {
       find: params[1],
-      replace: params[2]
-    }
+      replace: params[2],
+    };
   }
-  searchArray.forEach(item => {
+  searchArray.forEach((item) => {
     if (typeof item.find === 'string') {
-      item.find = [item.find];
+      find = [item.find];
     }
-    if (item.find.indexOf(string) > -1) {
+    if (find.indexOf(string) > -1) {
       final = item.replace;
     }
   });
+
   if (final === string && hash.fallback) {
     final = hash.fallback;
   }
